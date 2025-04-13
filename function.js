c1bbe16a01cf852a98e8f0eb41b1693aeebba2f9
Suraj Manoj Paswan <surajpaswanc0497@gmail.com>
2025-04-13 13:18:32 +0530

    Basic function use in JS

---
+++ function.js
@@ -0,0 +1,9 @@
+console.log("\t\tWriting a Basic function in Javascript\n");
+console.log("Function objective to print the name passed as Argument.\n");
+
+//Function definition.
+function  passingParameter(myname){
+	return myname;
+}
+
+console.log("\nArgument passed as parameter is-",passingParameter("Suraj"));
